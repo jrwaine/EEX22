74e65b4420ba953e69edad6e9fb14774e31b08af
Waine Junior <waine2000@gmail.com>
2019-10-19 19:48:58 -0300

    Adicionado teste do ultrassonico

--- src/integracao/main.py
+++ src/integracao/main.py
@@ -3,27 +3,51 @@ import portDefines as pd
 import RPi.GPIO as gpio
 import time
 
-def led_teste_loop():
-# led teste
+def led_test_loop():
+# led test
     time.sleep(0.5)
     gpio.output(pd.GPIO_PORT_OUT_LED, gpio.HIGH)
     time.sleep(0.5)
     gpio.output(pd.GPIO_PORT_OUT_LED, gpio.LOW)
 
-def buzzer_teste_loop():
+def buzzer_test_loop():
     gpio.output(pd.GPIO_PORT_OUT_BUZZER, gpio.HIGH)
     time.sleep(0.5)
     gpio.output(pd.GPIO_PORT_OUT_BUZZER, gpio.LOW)
+
+def ultrassonic_test_loop():
+    gpio.output(pd.GPIO_PORT_OUT_ULTR_TRIGG, gpio.HIGH)
+    time.sleep(0.00001)
+    gpio.output(pd.GPIO_PORT_OUT_ULTR_TRIGG, gpio.LOW)
+
+    pulse_end = 0
+    pulse_start = 0
+
+    while gpio.input(pd.GPIO_PORT_IN_ULTR_ECHO) == 0:
+        pulse_start = time.time()
+
+    while gpio.input(pd.GPIO_PORT_IN_ULTR_ECHO) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+
+    distance = round(distance, 2)
+
+    print("Distance: ", distance, "cm")
+    # time.sleep(1)
 
 try:
     cf.configGPIOs()
     cont_buzzer = 0
     while(1):
-        led_teste_loop()
-        cont_buzzer+=1
+        led_test_loop()
+        ultrassonic_test_loop()
+        cont_buzzer += 1
         if(cont_buzzer >= 5):
             cont_buzzer = 0
-            buzzer_teste_loop()
+            buzzer_test_loop()
 
 except KeyboardInterrupt:
     cf.resetGPIOs()
