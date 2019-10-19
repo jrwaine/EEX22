f3281d6daaab25421e5b422a20c4193acbb935a8
Waine Junior <waine2000@gmail.com>
2019-10-19 19:33:44 -0300

    Adicionado teste de buzzer

--- src/integracao/main.py
+++ src/integracao/main.py
@@ -10,9 +10,20 @@ def led_teste_loop():
     time.sleep(0.5)
     gpio.output(pd.GPIO_PORT_OUT_LED, gpio.LOW)
 
+def buzzer_teste_loop():
+    gpio.output(pd.GPIO_PORT_OUT_BUZZER, gpio.HIGH)
+    time.sleep(0.5)
+    gpio.output(pd.GPIO_PORT_OUT_BUZZER, gpio.LOW)
+
 try:
     cf.configGPIOs()
+    cont_buzzer = 0
     while(1):
         led_teste_loop()
+        cont_buzzer+=1
+        if(cont_buzzer >= 5):
+            cont_buzzer = 0
+            buzzer_teste_loop()
+
 except KeyboardInterrupt:
     cf.resetGPIOs()
