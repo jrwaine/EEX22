0b3222a13a4eb3616349fd335bf4f9145c678c1a
Waine Junior <waine2000@gmail.com>
2019-10-19 23:17:18 -0300

    Alterado codigo de teste da parafusadeira

--- src/integracao/main.py
+++ src/integracao/main.py
@@ -41,18 +41,15 @@ def ultrassonic_test_loop():
             return
 
     pulse_duration = pulse_end - pulse_start
-
     distance = pulse_duration * 17150
-
     distance = round(distance, 2)
-
     print("Distance: ", distance, "cm")
     if(distance < 10):
         buzz()
     #time.sleep(1)
 
-def servo_test_loop(cont):
 
+def servo_test_loop(cont):
     servo.ChangeDutyCycle(cont)
     time.sleep(0.5)
     if(cont == 0):
@@ -62,14 +59,15 @@ def servo_test_loop(cont):
     if(cont >= 10):
         cont = 0
 
+
 def motor_paraf_loop():
     gpio.output(pd.GPIO_PORT_OUT_PARAF_EN, gpio.HIGH)
     gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG1, gpio.LOW)
     gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG2, gpio.HIGH)
-    time.sleep(0.1)
+    time.sleep(2)
     gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG1, gpio.HIGH)
     gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG2, gpio.LOW)
-    time.sleep(0.1)
+    time.sleep(2.5)
     gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG1, gpio.LOW)
     gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG2, gpio.LOW)
 
