44d8ae265f51d1917372c53cdf2d9f6dd1711587
Giovanni Forastieri <cursedappleofsaggi@Giovannis-MacBook-Pro.local>
2019-10-26 18:26:04 -0300

    a

--- src/integracao/moveAGV.py
+++ src/integracao/moveAGV.py
@@ -6,39 +6,39 @@ import time
 FORWARD = 1
 BACKWARDS = -1
 
-motor = gpio.PWM(pd.GPIO_PORT_OUT_AGV_EN_PWM, 100)
-motor.start(0)
+class Motor():
+    def __init__(self):
+        self.encoder = Encoder()
+        self.motor = gpio.PWM(pd.GPIO_PORT_OUT_AGV_EN_PWM, 100)
+        self.motor.start(0)
 
-enco = Encoder()
+    def move(self, distance, velocity, direction=FORWARD):
+        if direction == FORWARD:
+            gpio.output(pd.GPIO_PORT_OUT_AGV_SIG1, gpio.HIGH)
+            gpio.output(pd.GPIO_PORT_OUT_AGV_SIG2, gpio.LOW)
+        else:
+            gpio.output(pd.GPIO_PORT_OUT_AGV_SIG1, gpio.LOW)
+            gpio.output(pd.GPIO_PORT_OUT_AGV_SIG2, gpio.HIGH)
 
-def move(distance, velocity, direction=FORWARD):
-    if direction == FORWARD:
+        initial_position = self.encoder.data()
+        print("Posicao inicial", initial_position)
+        print(str(self.encoder.data() * direction) + ' - ' + str((distance + initial_position) * direction))
+
+        while self.encoder.data() * direction <= (distance + initial_position) * direction:
+            print("Posicao atual", self.encoder.data())
+            self.motor.ChangeDutyCycle(100)
+            time.sleep(.050)    
+            self.motor.ChangeDutyCycle(0)
+            time.sleep(.050)    
+
+        self.stop()
+
+    def stop(self):
+        print("Parando..")
         gpio.output(pd.GPIO_PORT_OUT_AGV_SIG1, gpio.HIGH)
-        gpio.output(pd.GPIO_PORT_OUT_AGV_SIG2, gpio.LOW)
-    else:
-        gpio.output(pd.GPIO_PORT_OUT_AGV_SIG1, gpio.LOW)
         gpio.output(pd.GPIO_PORT_OUT_AGV_SIG2, gpio.HIGH)
-
-    initial_position = enco.data()
-    print("Posicao inicial", initial_position)
-    print(str(enco.data() * direction) + ' - ' + str((distance + initial_position) * direction))
-
-    while enco.data() * direction <= (distance + initial_position) * direction:
-        print("Posicao atual", enco.data())
-        motor.ChangeDutyCycle(100)
-        time.sleep(.050)    
-        motor.ChangeDutyCycle(0)
-        time.sleep(.050)    
-
-    stop()
-
-def stop():
-    print("Parando..")
-    gpio.output(pd.GPIO_PORT_OUT_AGV_SIG1, gpio.HIGH)
-    gpio.output(pd.GPIO_PORT_OUT_AGV_SIG2, gpio.HIGH)
-    motor.ChangeDutyCycle(100)
-    time.sleep(.300)
-    gpio.output(pd.GPIO_PORT_OUT_AGV_SIG1, gpio.LOW)
-    gpio.output(pd.GPIO_PORT_OUT_AGV_SIG2, gpio.LOW)
-    motor.ChangeDutyCycle(0)
-    
+        self.motor.ChangeDutyCycle(100)
+        time.sleep(.300)
+        gpio.output(pd.GPIO_PORT_OUT_AGV_SIG1, gpio.LOW)
+        gpio.output(pd.GPIO_PORT_OUT_AGV_SIG2, gpio.LOW)
+        self.motor.ChangeDutyCycle(0)
