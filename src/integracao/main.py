585147448f0f44a4fe76240ab5714e35a113a3c0
Eduardo <edu_yoshio27@hotmail.com>
2019-10-26 17:38:59 -0300

    SLEEPTEST

--- src/integracao/main.py
+++ src/integracao/main.py
@@ -10,8 +10,7 @@ def upParafusadeira():
         gpio.output(pd.GPIO_PORT_OUT_PARAF_EN, gpio.HIGH) #enable on
         gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG1, gpio.HIGH)
         gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG2, gpio.LOW)
-        while(gpio.input(pd.GPIO_PORT_IN_FDC_UPPER) == gpio.HIGH): #enquanto chave desapertada
-            continue
+        time.sleep(1)
         gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG1, gpio.LOW)
         gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG2, gpio.HIGH)
         gpio.output(pd.GPIO_PORT_OUT_PARAF_EN, gpio.LOW) #enable off
