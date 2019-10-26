bd7ea6ba367b5dbbd155f90c1b2c4c90c60af5fb
Giovanni Forastieri <cursedappleofsaggi@Giovannis-MacBook-Pro.local>
2019-10-26 19:16:00 -0300

    a

--- src/integracao/mainGigio.py
+++ src/integracao/mainGigio.py
@@ -3,8 +3,8 @@ from moveAGV import Motor
 
 try:
     motor = Motor()
-    motor.move(15, 60)
-    motor.move(15, 60)
+    motor.move(15)
+    motor.move(-15)
     cf.resetGPIOs()
 
 except KeyboardInterrupt:
