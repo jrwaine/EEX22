86677e896f2020dba4db095b867130a284fb9fb4
Eduardo <edu_yoshio27@hotmail.com>
2019-10-26 19:05:29 -0300

    testservo2

--- src/integracao/mainServo.py
+++ src/integracao/mainServo.py
@@ -1,12 +1,12 @@
 import config as cf
 import moveServo as servo
-
+import RPi.GPIO as gpio
 try:
-    servo = gpio.PWM(pd.GPIO_PORT_OUT_PWM_SERVO, 50) #50hz
+    servo = gpio.PWM(gpio.GPIO_PORT_OUT_PWM_SERVO, 50) #50hz
     servo.start(50)
     cont_servo = 50
     while(1):
-        cont_servo = moveServo(cont_servo)
+        cont_servo = servo.moveServo(cont_servo)
 
 except KeyboardInterrupt:
     cf.resetGPIOs()
