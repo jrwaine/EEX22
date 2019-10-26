0215eb59a180e4f386f3c095fca4242c49a25fa5
Eduardo <edu_yoshio27@hotmail.com>
2019-10-26 16:32:47 -0300

    funcao upParafusadeira

--- src/integracao/main.py
+++ src/integracao/main.py
@@ -4,6 +4,16 @@ import encoder as ec
 import RPi.GPIO as gpio
 import time
 
+
+def upParafusadeira():
+    gpio.output(pd.GPIO_PORT_OUT_PARAF_EN, gpio.HIGH) #enable on
+    gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG1, gpio.HIGH)
+    gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG2, gpio.LOW)
+    while(gpio.input(pd.GPIO_PORT_IN_FDC_UPPER) == gpio.HIGH):
+        continue
+    print("parafusadeira parada em cima")
+
+
 def led_test_loop():
     time.sleep(0.5)
     gpio.output(pd.GPIO_PORT_OUT_LED, gpio.HIGH)
@@ -62,7 +72,7 @@ def servo_test_loop(cont):
 
 def motor_paraf_loop():
     # descendo
-    gpio.output(pd.GPIO_PORT_OUT_PARAF_EN, gpio.HIGH)
+    gpio.output(pd.GPIO_PORT_OUT_PARAF_EN, gpio.HIGH) #enable on
     gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG1, gpio.LOW)
     gpio.output(pd.GPIO_PORT_OUT_PARAF_SIG2, gpio.HIGH)
     while(gpio.input(pd.GPIO_PORT_IN_FDC_LOWER) == gpio.HIGH):
@@ -120,6 +130,7 @@ try:
     cont_buzzer = 0
     cont_servo = 0
     while(1):
+        upParafusadeira()
         #led_test_loop()
         #ultrassonic_test_loop()
         #motor_paraf_loop()
