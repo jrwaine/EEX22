af4a2f46650f34524971e00ddfaf36f1a128048f
Giovanni Forastieri <cursedappleofsaggi@Giovannis-MacBook-Pro.local>
2019-10-26 15:15:20 -0300

    configuracao

--- src/integracao/config.py
+++ src/integracao/config.py
@@ -5,8 +5,8 @@ def configGPIOs():
     gpio.setmode(gpio.BOARD)
     
     # out GPIO
-    for i in pd.GPIO_PORTS_OUT:
-        gpio.setup(i, gpio.OUT)
+    for port in pd.GPIO_PORTS_OUT:
+        gpio.setup(port, gpio.OUT)
     
     # encoder
     gpio.setup(pd.GPIO_PORT_IN_ENC_SIG1, gpio.IN, pull_up_down=gpio.PUD_UP)
@@ -19,6 +19,9 @@ def configGPIOs():
     # ultrassonico
     gpio.setup(pd.GPIO_PORT_IN_ULTR_ECHO, gpio.IN)
 
+    # mantem as portas de saida em nivel baixo por padrao inicialmente
+    for port in pd.GPIO_PORTS_OUT:
+        gpio.output(port, gpio.LOW)
 
 def resetGPIOs():
     gpio.cleanup()
