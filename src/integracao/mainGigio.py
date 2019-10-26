033587a1d13eaf77f6ee9517f4a11854f78154af
Giovanni Forastieri <cursedappleofsaggi@Giovannis-MacBook-Pro.local>
2019-10-26 18:20:40 -0300

    a

--- src/integracao/mainGigio.py
+++ src/integracao/mainGigio.py
@@ -1,16 +1,9 @@
 import config as cf
 import moveAGV as motor
-import encoder
-from threading import Thread
 
-if __name__ == '__main__':
-    th = Thread(target = encoder.check)
-    print(th)
-    th.start()
+try:
+    motor.move(15, 60)
+    cf.resetGPIOs()
 
-    try:
-        motor.move(15, 60)
-        cf.resetGPIOs()
-
-    except KeyboardInterrupt:
-        cf.resetGPIOs()
+except KeyboardInterrupt:
+    cf.resetGPIOs()
