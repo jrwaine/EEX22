11bb7d352ba9b526d646920700325e98adc62db0
Giovanni Forastieri <cursedappleofsaggi@Giovannis-MacBook-Pro.local>
2019-10-26 23:04:10 -0300

    a

--- src/integracao/mainGigio.py
+++ src/integracao/mainGigio.py
@@ -1,17 +1,14 @@
 import config as cf
 from automaticGuidedVehicle import AGV
-from ultrassonico import Ultrassonico
-import time
 
 try:
-    # agv = AGV()
+    agv = AGV()
 
-    # agv.move(25, 'MEIO')
-    # agv.apertar(30)
-    # agv.move(15)
-    # agv.inicio()
-    ultra = Ultrassonico()
-    time.sleep(20)
+    agv.move(25, 'MEIO')
+    agv.apertar(30)
+    agv.move(15)
+    agv.inicio()
+    
 
     
     cf.resetGPIOs()
