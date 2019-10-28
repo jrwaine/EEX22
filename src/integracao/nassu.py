7274bb86849019cacb5b00c165491913c3ab8d3c
Giovanni Forastieri <cursedappleofsaggi@Giovannis-MacBook-Pro.local>
2019-10-28 11:12:12 -0300

    a

--- src/integracao/nassu.py
+++ src/integracao/nassu.py
@@ -3,12 +3,13 @@ from automaticGuidedVehicle import AGV
 try:
     agv = AGV()
 
-    agv.move(25, 'CIMA')
-    agv.apertar(agv.verificar_parafuso())
-    agv.move(10)
-    agv.move(25, 'MEIO')
-    agv.apertar(90)
-    agv.inicio()
+    agv.parafusadeira.subir()
+    # agv.move(25, 'CIMA')
+    # agv.apertar(agv.verificar_parafuso())
+    # agv.move(10)
+    # agv.move(25, 'MEIO')
+    # agv.apertar(90)
+    # agv.inicio()
     
     agv.stop()
     
