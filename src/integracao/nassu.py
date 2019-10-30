545c89e354a982710acb36a04e40688429a62400
Giovanni Forastieri <cursedappleofsaggi@Giovannis-MacBook-Pro.local>
2019-10-30 16:51:26 -0300

    a

--- src/integracao/nassu.py
+++ src/integracao/nassu.py
@@ -11,11 +11,11 @@ try:
     # while True:
     #     print('oioi')
     #     time.sleep(1)
-    
-    agv.move(25, 'CIMA')
+
+    agv.move(35, 'MEIO')
     agv.apertar(agv.verificar_parafuso())
     agv.move(10)
-    agv.move(25, 'MEIO')
+    agv.move(35, 'MEIO')
     agv.apertar(90)
     agv.inicio()
 
