2c616c22e1b1ee0c4bda7743862de0f1ad271c19
Giovanni Forastieri <cursedappleofsaggi@Giovannis-MacBook-Pro.local>
2019-10-30 16:28:44 -0300

    a

--- src/integracao/nassu.py
+++ src/integracao/nassu.py
@@ -4,24 +4,22 @@ from ultrassonico import Ultrassonico
 import time
 
 try:
-    # agv = AGV()
+    agv = AGV()
 
-    ultra = Ultrassonico()
-    print('kk ear ', ultra.is_alive())
-    while True:
-        print('oioi')
-        time.sleep(1)
-
-    # for i in range(3):
-    #     agv.movimentation.brake()
-    #     agv.move(25, 'CIMA')
-    #     agv.apertar(agv.verificar_parafuso())
-    #     agv.move(10)
-    #     agv.move(25, 'MEIO')
-    #     agv.apertar(90)
-    #     agv.inicio()
+    # ultra = Ultrassonico()
+    # print('kk ear ', ultra.is_alive())
+    # while True:
+    #     print('oioi')
+    #     time.sleep(1)
     
-    #     agv.stop()
+    agv.move(25, 'CIMA')
+    agv.apertar(agv.verificar_parafuso())
+    agv.move(10)
+    agv.move(25, 'MEIO')
+    agv.apertar(90)
+    agv.inicio()
+
+    agv.stop()
     
 except KeyboardInterrupt:
     agv.stop()
