cb5420c0ab570be776a45dfedb7e31673f995f75
NEOGIGIO <gigio.forastieri@gmail.com>
2019-11-02 16:05:27 -0300

    a

--- src/integracao/main.py
+++ src/integracao/main.py
@@ -7,7 +7,7 @@ agv = None
 try:
     agv = AGV()
 
-    agv.move(20, globals.MEIO)
+    agv.move(25, globals.MEIO)
     agv.apertar(agv.verificar_parafuso())
     agv.move(10, globals.CIMA)
     agv.move(25, globals.MEIO)
