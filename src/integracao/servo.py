6137a3588e78602e6e82fd86821dd22c4e55b544
NEOGIGIO <gigio.forastieri@gmail.com>
2019-11-02 15:37:03 -0300

    a

--- src/integracao/servo.py
+++ src/integracao/servo.py
@@ -10,6 +10,9 @@ class Servo:
         self.position = globals.MIN
         self.pwm = gpio.PWM(ports.GPIO_PORT_OUT_PWM_SERVO, 50)
         self.pwm.start(0)
+        self.setAngle(180)
+        time.sleep(1)
+        self.setAngle(0)
 
     def apertar(self):
         time.sleep(0.5)
