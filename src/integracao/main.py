7de31c5cade2b3a23135a6c7c1ad70c3e0d506e1
NEOGIGIO <gigio.forastieri@gmail.com>
2019-11-02 21:24:17 -0300

    a

--- src/integracao/main.py
+++ src/integracao/main.py
@@ -14,10 +14,10 @@ try:
     agv = AGV()
 
     agv.move(25, globals.MEIO)
-    # agv.apertar(agv.verificar_parafuso())
-    # agv.move(10, globals.CIMA)
-    # agv.move(25, globals.MEIO)
-    # agv.apertar(90)
+    agv.apertar(agv.verificar_parafuso())
+    agv.move(10, globals.CIMA)
+    agv.move(25, globals.MEIO)
+    agv.apertar(90)
     agv.inicio()
 
     agv.kill()
