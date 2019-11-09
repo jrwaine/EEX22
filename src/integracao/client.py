78c04668c8d488b4866d6e9642e1bcffd80475cb
NEOGIGIO <gigio.forastieri@gmail.com>
2019-11-09 18:59:42 -0300

    a

--- src/integracao/client.py
+++ src/integracao/client.py
@@ -6,10 +6,17 @@ import json
 HOST = '192.168.100.74'  # The server's hostname or IP address
 PORT = 9077        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    data = s.recv(1024)
-    b = b''
-    b += data
-    d = json.loads(b.decode('utf-8'))
-    print(d)
+while True:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            data = s.recv(1024)
+            b = b''
+            b += data
+            d = json.loads(b.decode('utf-8'))
+            print(d)
+            break
+    except:
+        pass
+
+
