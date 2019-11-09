4b1be23b7313fd85489207c2a811a8649119d441
NEOGIGIO <gigio.forastieri@gmail.com>
2019-11-09 18:29:11 -0300

    a

--- src/integracao/server.py
+++ src/integracao/server.py
@@ -8,8 +8,8 @@ import json
 init_time = time.time()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print(socket.gethostname())
-    s.bind((socket.gethostname(), globals.PORT))
+    print(socket.gethostbyname(socket.gethostname()))
+    s.bind((socket.gethostbyname(socket.gethostname()), globals.PORT))
     while time.time() - init_time < globals.COMMUNICATION_LIMIT_TIME:
         s.listen()
         conn, addr = s.accept()
